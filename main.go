8d6f750f07b0c6a1b225b07df4034af943863bcd
Jing Dong <jing.dong@activars.com>
2014-08-04 17:47:26 +0100

    moving marathon to it's own package

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
     "io"
     "net/http"
+	"encoding/json"
     "github.com/zenazn/goji"
     "github.com/zenazn/goji/web"
+	"./marathon"
 )
 
 func hello(c web.C, w http.ResponseWriter, r *http.Request) {
@@ -21,10 +23,16 @@ func haproxyConfigUpdateHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "haproxy updated!")
 }
 
-//
+
+func marathonAppsHandler(w http.ResponseWriter, r *http.Request) {
+	apps, _ := marathon.Apps("http://aws.fe-marathon.qutics.com:8080")
+	payload, _ := json.Marshal(apps)
+	io.WriteString(w, string(payload))
+}
 /* HTTP Service */
 func main() {
 	goji.Get("/status", Status)
 	goji.Post("/api/haproxy/update", haproxyConfigUpdateHandler)
+	goji.Get("/api/marathon/apps", marathonAppsHandler)
 	goji.Serve()
 }
