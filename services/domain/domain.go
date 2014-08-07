4af5036a5ebfee3adfcbcac7e45f57b2d15ea61e
Jing Dong <jing.dong@activars.com>
2014-08-07 18:25:04 +0100

    hook api with domain struct

--- services/domain/domain.go
+++ services/domain/domain.go
@@ -1,26 +1,31 @@
 package domain
 
 import (
+	"github.com/samuel/go-zookeeper/zk"
 	conf "bamboo/configuration"
 )
 
 /*
 	Get an  key value
  */
-func FetchAll(zkConf conf.Zookeeper) (map[string]string, error) {
+func All(conn *zk.Conn, zkConf conf.Zookeeper) (map[string]string, error) {
+//	zkConf.Path
+
 	return map[string]string{}, nil
 }
 
 
-func Create(zkConf conf.Zookeeper, appName string, domainValue string) error {
+func Create(conn *zk.Conn, zkConf conf.Zookeeper, appId string, domainValue string) error {
+//	createRq := CreateRequest{ Path: zkConf.Path }
+//	ops := zk.MultiOps{ []CreateRequest: createRq }
 	return nil
 }
 
-func Update(zkConf conf.Zookeeper, appName string, domainValue string) error {
+func Put(conn *zk.Conn, zkConf conf.Zookeeper, appId string, domainValue string) error {
 	return nil
 }
 
-func Delete(zkConf conf.Zookeeper, appName string) error {
+func Delete(conn *zk.Conn, zkConf conf.Zookeeper, appId string) error {
 	return nil
 }
 //
