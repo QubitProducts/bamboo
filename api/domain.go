d66246d114315c172eaa7ed30c1b1683e1fd0637
Jing Dong <jing.dong@activars.com>
2014-08-07 18:26:39 +0100

    updated gitignore

--- api/domain.go
+++ api/domain.go
@@ -1,16 +1,31 @@
 package api
 
-func HandleCreateDomain() {
-//	domain.FetchAll()
+import(
+	"net/http"
+
+	"github.com/samuel/go-zookeeper/zk"
+	conf "bamboo/configuration"
+)
+
+type Domain struct {
+	Config conf.Configuration
+	Zookeeper zk.Conn
 }
 
-func HandleUpdateDomain() {
-//	domain.Update()
-}
-
-func HandleDeleteDomain() {
+func (d Domain) All(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func (d Domain) Create(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (d Domain) Delete(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (d Domain) Put(w http.ResponseWriter, r *http.Request) {
+
+}
 
 
