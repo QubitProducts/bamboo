6909f109f37f1d050142aa39dce90d4b95e93768
Pedro Pinheiro <pdpinheiro@gmail.com>
2014-08-07 17:08:59 +0100

    Refactor HAProxy so it keeps its state in a struct

--- api/state.go
+++ api/state.go
@@ -1,14 +1,22 @@
 package api
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
-	"encoding/json"
 
+	"github.com/samuel/go-zookeeper/zk"
+
+	"bamboo/configuration"
 	"bamboo/services/haproxy"
 )
 
-func HandleState(w http.ResponseWriter, r *http.Request) {
-	payload, _ := json.Marshal(haproxy.GetTemplateData())
+type State struct {
+	Config    configuration.Configuration
+	Zookeeper zk.Conn
+}
+
+func (state State) Get(w http.ResponseWriter, r *http.Request) {
+	payload, _ := json.Marshal(haproxy.GetTemplateData(state.Config))
 	io.WriteString(w, string(payload))
 }
--- services/haproxy/haproxy.go
+++ services/haproxy/haproxy.go
@@ -1,29 +1,26 @@
 package haproxy
 
-import(
+import (
 	"bamboo/services/marathon"
 
 	conf "bamboo/configuration"
 	writer "bamboo/writer"
-	"bamboo/services/cmd"
 )
 
-
 type templateData struct {
-	Apps []marathon.App
+	Apps     []marathon.App
 	Services map[string]string
 }
 
-func WriteHAProxyConfig(haproxyConf conf.HAProxy, data interface {}) error {
+func WriteHAProxyConfig(haproxyConf conf.HAProxy, data interface{}) error {
 	return writer.WriteTemplate(haproxyConf.TemplatePath, haproxyConf.OutputPath, data)
 }
 
-func GetTemplateData() interface {} {
-	config := cmd.GetConfiguration()
+func GetTemplateData(config conf.Configuration) interface{} {
 
 	apps, _ := marathon.FetchApps(config.Marathon.Endpoint)
 	//	services, _ := domain.FetchAll(config.ServicesMapping.Zookeeper)
 	services := map[string]string{}
 
-	return templateData{ apps, services }
+	return templateData{apps, services}
 }
