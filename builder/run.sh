8a9788b5a54a7f53e604c3d56f81110ac22c3486
Jing Dong <jing.dong@activars.com>
2014-11-09 00:55:53 +0000

    Remove unused enviroment variables for haproxy, this should be configured in template

--- builder/run.sh
+++ builder/run.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-
-if [ -z "$HAPROXY_USERNAME" ]; then
-    export HAPROXY_USERNAME="admin"
-fi
-
-if [ -z "$HAPROXY_URI" ]; then
-    export HAPROXY_URI="/haproxy?stats"
-fi
-
-if [ -n "$HAPROXY_PASSWORD" ]
-then
-  echo "  stats enable" >> /etc/haproxy/haproxy.cfg
-  echo "  stats auth $HAPROXY_USERNAME:$HAPROXY_PASSWORD" >> /etc/haproxy/haproxy.cfg
-  echo "  stats uri $HAPROXY_URI" >> /etc/haproxy/haproxy.cfg
-fi
-
 haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid
-
 /usr/bin/supervisord
