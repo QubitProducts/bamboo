f447676091a365791e56893018e81b3da9cceffa
John Alberts <john.m.alberts@gmail.com>
2015-01-26 10:59:28 -0800

    Use container $HOST environment variable as BAMBOO_ENDPOINT if AUTO_BAMBOO_HOST env var is set. This is necessary since we can't simply set BAMBOO_ENDPOINT=$HOST in Marathon as Marathon doesn't seem to support parameter substitution for environment variable values.

--- builder/run.sh
+++ builder/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if [[ -n $AUTO_BAMBOO_HOST ]]; then
+sed -i "s/^.*Endpoint\": \"\(http:\/\/haproxy-ip-address:8000\)\".*$/    \"EndPoint\": \"$HOST\",/" \
+    ${CONFIG_PATH:=config/production.example.json}
+fi
 haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid
 /usr/bin/supervisord
