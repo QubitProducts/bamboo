197d01dc9ef4a2202198f38ab6869df52ede0f10
John Alberts <john.m.alberts@gmail.com>
2015-01-27 08:18:05 -0800

    Add port 8000 to sed command

--- builder/run.sh
+++ builder/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ -n $AUTO_BAMBOO_HOST ]]; then
-sed -i "s/^.*Endpoint\": \"\(http:\/\/haproxy-ip-address:8000\)\".*$/    \"EndPoint\": \"$HOST\",/" \
+sed -i "s/^.*Endpoint\": \"\(http:\/\/haproxy-ip-address:8000\)\".*$/    \"EndPoint\": \"$HOST:8000\",/" \
     ${CONFIG_PATH:=config/production.example.json}
 fi
 haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid
