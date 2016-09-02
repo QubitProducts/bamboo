431d2390df9a9a238b13d037228ab9b2c2cf46e5
JING DONG <me@jing.io>
2016-09-02 23:48:05 +0100

    Fix fmt format

--- services/marathon/marathon.go
+++ services/marathon/marathon.go
@@ -69,14 +69,14 @@ type HealthCheckResult struct {
 }
 
 type marathonTask struct {
-	AppId        string
-	Id           string
-	Host         string
-	Ports        []int
-	ServicePorts []int
-	StartedAt    string
-	StagedAt     string
-	Version      string
+	AppId              string
+	Id                 string
+	Host               string
+	Ports              []int
+	ServicePorts       []int
+	StartedAt          string
+	StagedAt           string
+	Version            string
 	HealthCheckResults []HealthCheckResult
 }
 
@@ -194,14 +194,14 @@ func fetchTasks(endpoint string, conf *configuration.Configuration) (map[string]
 func calculateTaskHealth(healthCheckResults []HealthCheckResult, healthChecks []HealthChecks) bool {
 	//If we don't even have health check results for every health check, don't count the task as healthy
 	if len(healthChecks) > len(healthCheckResults) {
-		return false;
+		return false
 	}
 	for _, healthCheck := range healthCheckResults {
 		if !healthCheck.Alive {
-			return false;
+			return false
 		}
 	}
-	return true;
+	return true
 }
 
 func createApps(tasksById map[string]marathonTaskList, marathonApps map[string]marathonApp) AppList {
